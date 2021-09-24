cddb89989f73fea79419ab4033bb5eb817fdfd62
BUBU <cris.cadosh@gmail.com>
2021-09-24 15:34:28 -0400

    sound box

---
+++ app.js
@@ -0,0 +1,30 @@
+const sounds = ['bird', 'cat', 'cow', 'dog', 'pig', 'sheep'];
+
+
+sounds.forEach(sound =>{
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', ()=>{
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+
+function stopSongs(){
+    sounds.forEach(sound =>{
+        
+        const song = document.getElementById(sound)
+        
+        song.pause()
+        song.currentTime = 0;
+    })
+}
